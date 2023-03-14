17d248d2b78f3a65454a5857df954f35bc1fb500
[AI6/1반] 박유경 <ewig1006@gmail.com>
2023-03-14 06:19:48 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const sequelize = require('./src/databases/sequelize');
 const shelterRouter = require('./src/routes/shelterRouter');
-const cvstojson = require('./assets/cvsToJson');
+const csvtojson = require('./assets/csvToJson');
 const app = express();
 
 app.use(express.json());
 app.use('/shelter', shelterRouter);
 
-//cvstojson(); // json 파일로 변경시 주석해제하고 사용해주세요 
-//assets/files에 cvs파일 넣고 cvsToJson.js에서 파일명 변경후 사용하시면 됩니다.
+//csvtojson(); // json 파일로 변경시 주석해제하고 사용해주세요 
+//assets/files에 csv파일 넣고 csvToJson.js에서 파일명 변경후 사용하시면 됩니다.
 
 const PORT = process.env.PORT || 3000;
 
